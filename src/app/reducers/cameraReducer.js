931ab45d26b3778187730327aa3d36f3d485edbc
xx18 <xx18@global.tesco.org>
2018-07-29 12:05:27 +0530

    camera reducer set up

---
+++ src/app/reducers/cameraReducer.js
@@ -0,0 +1,12 @@
+import initialState from './initialState';
+
+export default function cameraReducer(state = initialState.cameraList, action) {
+
+switch(action.type){
+    case "FETCH_DRONE_CAMERAS":
+    default:
+            return state;
+
+}
+
+}
--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import cameraReducer from './cameraReducer';
 
 const rootReducer = combineReducers({
-    routing: routerReducer
+    routing: routerReducer,
+    camera:cameraReducer
 });
 
 export default rootReducer;
